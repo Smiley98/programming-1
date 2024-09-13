99977b2f4516d91faa3ba7f319ebe8aac8d96406
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-13 12:43:44 -0400

    Week 2 complete

--- Assets/Scripts/MathOperations.cs
+++ Assets/Scripts/MathOperations.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
-public class CoolScript : MonoBehaviour
+public class MathOperations : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //int number = 420;
+        //Debug.Log(number);      // Logs 420
+        //Debug.Log("number");    // Logs number
+
         int health = 100;
         int defense = 10;
         int attack = 50;
@@ -12,25 +15,20 @@ public class CoolScript : MonoBehaviour
         int regeneration = 15;
         int damage = attack * speed / defense;
 
-        Debug.Log("Initial Stats:");
-        Debug.Log("Attack: " + attack);
+        Debug.Log("---Logging Combat Stats---");
+        Debug.Log("Health: " + health);
         Debug.Log("Defense: " + defense);
+        Debug.Log("Attack: " + attack);
         Debug.Log("Speed: " + speed);
         Debug.Log("Damage: " + damage);
         Debug.Log("Regeneration: " + regeneration);
 
-        Debug.Log("Applying " + damage + " damage.");
+        Debug.Log("Applying damge of " + damage + " to health of " + health);
         health = health - damage;
         Debug.Log("Health is now " + health);
 
-        Debug.Log("Applying " + regeneration + " regeneration.");
+        Debug.Log("Applying regeneration of " + regeneration + " to health of " + health);
         health = health + regeneration;
         Debug.Log("Health is now " + health);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ Assets/Scripts/Medals.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Medals : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Top 3 Video Games:");
+        Debug.Log("Mario Kart Wii");
+        Debug.Log("Super Smash Bros Melee");
+        Debug.Log("MineCraft");
+    }
+}
