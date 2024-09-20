35d7eb1551af2bb416d404e41725065f5d3eb641
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-20 13:27:44 -0400

    Added movement along x-axis

--- Assets/Scripts/DrawAxes.cs
+++ Assets/Scripts/DrawAxes.cs
@@ -19,5 +19,10 @@ public class DrawAxes : MonoBehaviour
         Debug.DrawLine(x1, x2, Color.red);
         Debug.DrawLine(y1, y2, Color.green);
         Debug.DrawLine(z1, z2, Color.blue);
+
+        float dt = Time.deltaTime;
+        float speed = 2.0f;
+        Vector3 velocity = xDirection * speed * dt;
+        transform.position = transform.position + velocity;
     }
 }
