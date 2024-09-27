00469b115b48a1db13e11208e920ab8923da8d06
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-27 13:15:33 -0400

    Completed week 4

--- Assets/Scripts/BattleTest.cs
+++ Assets/Scripts/BattleTest.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class BattleTest : MonoBehaviour
 {
+    // See homework 3 document for Week 4 homework!
     void Start()
     {
         Debug.Log("Prepare for battle!");
+
+        int playerHealth = 100;
+        int enemyHealth = 50;
+
+        if (playerHealth > enemyHealth)
+        {
+            // If the player has more health than the enemy, the "true" case runs
+            Debug.Log("Begone foul fiend!!!");
+        }
+        else
+        {
+            // Otherwise, if the enemy has more health than the player, the "false" case runs
+            Debug.Log("Begone pathetic player ;p ;p");
+        }
     }
 }
