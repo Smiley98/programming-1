2496f5d107d54549bbe53f8636e6c8d4d8741150
Smiley98 <smileysofficialcinema@gmail.com>
2024-10-04 12:25:46 -0400

    Added ball movement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    Vector2 direction = Vector2.right;
+    float speed = 10.0f;
+
     void Start()
     {
-        Debug.Log("Behold... THE BALL!!!!!!");
+        // Uncomment to launch the ball at an angle
+        //float angle = 30.0f * Mathf.Deg2Rad;
+        //direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
     }
 
     void Update()
     {
-        
+        float dt = Time.deltaTime;
+        Vector3 change = direction * speed * dt;
+        transform.position = transform.position + change;
     }
 }
