bd39ebcd97f9e3ab1e1d3aeec6b3744c7906d19f
Smiley98 <smileysofficialcinema@gmail.com>
2024-10-04 12:12:15 -0400

    Week 5 start

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Behold... THE BALL!!!!!!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
