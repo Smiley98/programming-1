7667eb23455a40112e5785b1f39aa8bded791864
Smiley98 <smileysofficialcinema@gmail.com>
2024-11-22 12:40:30 -0500

    Removed homework7 script

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public class Functions : MonoBehaviour
 {
-    // "int" = data-type, "example" = variable's name
-    int example = 5;
-
-    // "void" = no output, "Start" = function's name
     void Start()
     {
         int number1 = 5;
@@ -16,38 +12,33 @@ public class Functions : MonoBehaviour
         Debug.Log(difference);
 
         float[] numbers = { 4.4f, 7.7f, 2.2f, 3.3f, 6.6f, 9.9f, 8.8f };
-        float largestNumber = FindHighest(numbers);
-        Debug.Log(largestNumber);
+        float highest = FindHighest(numbers);
+        Debug.Log(highest);
 
-        // 1. Create a Mul function that multiplies 2 double-precision numbers
-        // 2. Create a Div function that divides 2 double-precision numbers
-        // 3. Create a FindLowest function that finds the lowest number within an array
+        // 1. Make a function called Mul that multiplies 2 double-precision numbers
+        // 2. Make a function called Div that divides 2 double-precision numbers
+        // 3. Make a function called FindLowest that finds the lowest number in an array
     }
 
-    // "int" = output, "Add" = name, "a and b" = inputs 1 & 2
+    // "int" = output, "Add" = name, "int a and int b" = two integer inputs
     int Add(int a, int b)
     {
-        // Output the result of a + b ("return" = output)
         return a + b;
     }
 
-    // "int" = output, "Sub" = name, "a and b" = inputs 1 & 2
+    // "int" = output, "Sub" = name, "int a and int b" = two integer inputs
     int Sub(int a, int b)
     {
-        // Output the result of a - b ("return" = output)
         return a - b;
     }
 
-    // "float" = output, "FindHighest" = name, "numbers" = input
     float FindHighest(float[] numbers)
     {
         float highest = float.MinValue;
         for (int i = 0; i < numbers.Length; i++)
         {
             if (numbers[i] > highest)
-            {
                 highest = numbers[i];
-            }
         }
         return highest;
     }
--- Assets/Scripts/Homework7.cs
+++ Assets/Scripts/Homework7.cs
@@ -2,37 +2,9 @@ using UnityEngine;
 
 public class Homework7 : MonoBehaviour
 {
-    // 1. Determine highest number
-    // 2. Determine the lowest number
-    // 3. Determine the index of the highest number
-    // 4. Determine the index of the lowest number
+    
     void Start()
     {
-        float[] numbers = { 4.4f, 7.7f, 2.2f, 3.3f, 6.6f, 9.9f, 8.8f };
-
-        float highest = float.MinValue;
-        float lowest = float.MaxValue;
-        int highestIndex = -1;
-        int lowestIndex = -1;
-
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            float value = numbers[i];
-            if (value > highest)
-            {
-                highest = value;
-                highestIndex = i;
-            }
-            else if (value < lowest)
-            {
-                lowest = value;
-                lowestIndex = i;
-            }
-        }
-
-        Debug.Log("Highest number: " + highest);
-        Debug.Log("Lowest number: " + lowest);
-        Debug.Log("Highest number index: " + highestIndex);
-        Debug.Log("Lowest number index: " + lowestIndex);
+        
     }
 }
